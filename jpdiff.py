d5df0d09670e5dd8ff0ec0943007fe720c941db8
Dan Davison <dandavison7@gmail.com>
2011-10-09 18:10:37 -0700

    Simple diff tool for key\tvalue output, e.g. from jsonpipe

---
+++ jpdiff.py
@@ -0,0 +1,18 @@
+import sys
+from itertools import izip
+
+EPSILON = 1e-11
+
+
+if __name__ == '__main__':
+    for pair in izip(*map(open, sys.argv[1:])):
+        (key1, val1), (key2, val2)  = [line.split('\t') for line in pair]
+        if val1 != val2:
+            try:
+                if abs(float(val1) - float(val2)) < EPSILON:
+                    pass
+                else:
+                    print ''.join(pair)
+            except ValueError:
+                print ''.join(pair)
+                raise
